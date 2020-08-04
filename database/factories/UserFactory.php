33a133db33c5c0e07b569293f60f63a5d5d723ba
amir566 <amir.amir566.aj@gmail.com>
2020-08-04 16:36:19 +0430

    database design v 0

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,11 +18,103 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $created_at = $faker->dateTimeBetween('-1 year', 'now');
+    $ngseed = rand(0, 1);
+
+    $name = '';
+    $gender = '';
+    $birthday = $faker->dateTimeBetween('-60 year','-1 months');
+    switch ($ngseed) {
+        case 0:
+            $name = $faker->firstNameFemale;
+            $gender = 'زن';
+            break;
+        case 1:
+            $name = $faker->firstNameMale;
+            $gender = 'مرد';
+            break;
+    }
+
+
+    $msseed = rand(0, 1);
+    $marriage_status = '';
+    $wedding_anniversary = null;
+    switch ($msseed) {
+        case 0:
+            $marriage_status = 'مجرد';
+            break;
+        case 1:
+            $marriage_status = 'متاهل';
+            $wedding_anniversary = $faker->dateTimeBetween('-42 year', 'now');
+            break;
+
+    }
+    $edseed = rand(0, 5);
+    $education = '';
+    switch ($edseed) {
+        case 0:
+            $education = 'زیر دیپلم';
+            break;
+        case 1:
+            $education = 'دیپلم';
+            break;
+        case 2:
+            $education = 'فوق دیپلم';
+            break;
+        case 3:
+            $education = 'لیسانس';
+            break;
+        case 4:
+            $education = 'فوق لیسانس';
+            break;
+        case 5:
+            $education = 'دکترا';
+            break;
+    }
+
+    $updated_at_seed = rand(0, 5);
+    $updated_at = null;
+    if ($updated_at_seed == 0) {
+        $updated_at = $faker->dateTimeBetween($created_at, 'now');
+    }
+    $is_active_seed = rand(0, 5);
+    $is_active = 1;
+    if ($is_active_seed > 0) {
+        $is_active = 0;
+    }
+    $is_deleted_seed = rand(0, 5);
+    $is_deleted = 0;
+    if ($is_deleted_seed > 0) {
+        $is_deleted = 0;
+    }
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'name' => $name,
+        'family' => $faker->lastName,
+        'father_name' => $faker->firstNameMale,
+        'national_code'=>$faker->numberBetween(00000000000, 9999999999),
+        'gender' => $gender,
+        'mobile' => '09' . $faker->unique()->numberBetween(111111111, 999999999),
+        'mobile_verified_at' => $faker->dateTimeBetween($created_at, 'now'),
+        'phone' => '021' . $faker->numberBetween(11111111, 99999999),
+        'email' => $faker->unique()-> email,
+        'email_verified_at' => $faker->dateTimeBetween($created_at, 'now'),
+        'have_social' => $faker->numberBetween(0, 1),
+        'birthday' => $birthday,
+        'marriage_status' => $marriage_status,
+        'wedding_anniversary' => $wedding_anniversary,
+        'education' => $education,
+        'field' => $faker->jobTitle,
+        'address' => $faker->unique()->address,
+        'registration_origin' => 1,
+        'website'=>'www.'.$faker->company.'.com',
+        'skill'=>$faker->jobTitle,
+        'card_number' => $faker->numberBetween(0001, 9999) . $faker->unique()->numberBetween(111111111111, 999999999999),
+        'businesse_id'=>1,
+        'role_id'=>1,
+        'user_ip'=>$faker->ipv4,
+        'password' => md5('secret'),
+        'updated_at' => $updated_at,
+        'is_active' => $is_active,
+        'is_deleted' => $is_deleted,
     ];
 });
